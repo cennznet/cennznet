77ec8cb16fd001584d3dd8ddadd21cf78b6a3b0c
Xiliang Chen <xlchen1291@gmail.com>
2019-02-13 10:43:43 +1300

    bump version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -55,8 +55,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("cennznet"),
 	impl_name: create_runtime_str!("centrality-cennznet"),
 	authoring_version: 1,
-	spec_version: 10,
-	impl_version: 10,
+	spec_version: 11,
+	impl_version: 11,
 	apis: RUNTIME_API_VERSIONS,
 };
 
