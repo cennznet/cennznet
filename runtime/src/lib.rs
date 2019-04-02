6687857dc69387f9ce21669a6d0f7b7c8ac8a2ff
Xiliang Chen <xlchen1291@gmail.com>
2019-04-03 11:00:41 +1300

    bump version and update local and kauri genesis

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -60,8 +60,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("cennznet"),
 	impl_name: create_runtime_str!("centrality-cennznet"),
 	authoring_version: 1,
-	spec_version: 16,
-	impl_version: 16,
+	spec_version: 17,
+	impl_version: 17,
 	apis: RUNTIME_API_VERSIONS,
 };
 
