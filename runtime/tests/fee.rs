b6fc61675f3385c35c8eac0aa5639282a8529c9a
Bryan Chen <xlchen1291@gmail.com>
2019-05-03 10:22:15 +1200

    format

--- runtime/tests/fee.rs
+++ runtime/tests/fee.rs
@@ -1,10 +1,10 @@
 //!
 //! Fee integration tests
 //!
-use cennznet_runtime::{Call, ExtrinsicFeePayment, Fee, Runtime};
 use cennznet_primitives::CheckedCennznetExtrinsic;
+use cennznet_runtime::{Call, ExtrinsicFeePayment, Fee, Runtime};
 use runtime_io::with_externalities;
-use runtime_primitives::{BuildStorage};
+use runtime_primitives::BuildStorage;
 use substrate_primitives::{sr25519::Public, Blake2Hasher};
 use support::{additional_traits::ChargeExtrinsicFee, assert_err, assert_ok};
 
