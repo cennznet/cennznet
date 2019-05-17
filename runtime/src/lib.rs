002d1039920758de798830b552d30603002b3283
Bryan Chen <xlchen1291@gmail.com>
2019-05-17 14:51:24 +1200

    Bump version to 24 beta

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -57,8 +57,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("cennznet"),
 	impl_name: create_runtime_str!("centrality-cennznet"),
 	authoring_version: 1,
-	spec_version: 23,
-	impl_version: 23,
+	spec_version: 24,
+	impl_version: 24,
 	apis: RUNTIME_API_VERSIONS,
 };
 
