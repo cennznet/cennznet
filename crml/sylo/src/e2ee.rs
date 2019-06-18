57d53d84cd36c014f1c8487f94235b752a3ed753
elim0322 <e.lim0322@gmail.com>
2019-06-18 12:47:12 +1200

    "Change sr-primitives -> runtime_primitives & substrate_primitives -> primitives for crml/sylo"

--- crml/sylo/src/e2ee.rs
+++ crml/sylo/src/e2ee.rs
@@ -17,8 +17,8 @@ use srml_support::{dispatch::Vec, StorageMap};
 use {device, groups, inbox, response, system, system::ensure_signed};
 
 extern crate sr_io;
-extern crate sr_primitives;
-extern crate substrate_primitives;
+extern crate runtime_primitives;
+extern crate primitives;
 
 const MAX_PKBS: usize = 50;
 
@@ -99,13 +99,13 @@ pub(super) mod tests {
 	use super::*;
 	use codec::{Decode, Encode};
 	use serde::{Deserialize, Serialize};
-	use primitives::traits::{Verify, Lazy};
+	use runtime_primitives::traits::{Verify, Lazy};
 
 	use self::sr_io::with_externalities;
-	use self::substrate_primitives::{Blake2Hasher, H256};
+	use self::primitives::{Blake2Hasher, H256};
 	// The testing primitives are very useful for avoiding having to work with signatures
 	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are requried.
-	use self::sr_primitives::{
+	use self::runtime_primitives::{
 		testing::{Digest, DigestItem, Header},
 		traits::{BlakeTwo256, IdentityLookup},
 		BuildStorage,
--- crml/sylo/src/inbox.rs
+++ crml/sylo/src/inbox.rs
@@ -18,10 +18,10 @@ use system::ensure_signed;
 
 extern crate sr_io;
 extern crate sr_std;
-extern crate substrate_primitives;
+extern crate primitives;
 
 // Needed for various traits. In our case, `OnFinalise`.
-extern crate sr_primitives;
+extern crate runtime_primitives;
 
 // `system` module provides us with all sorts of useful stuff and macros
 // depend on it being around.
@@ -117,13 +117,13 @@ mod tests {
 
 	use codec::{Decode, Encode};
 	use serde::{Deserialize, Serialize};
-	use primitives::traits::{Verify, Lazy};
+	use runtime_primitives::traits::{Verify, Lazy};
 
 	use self::sr_io::with_externalities;
-	use self::substrate_primitives::{Blake2Hasher, H256};
+	use self::primitives::{Blake2Hasher, H256};
 	// The testing primitives are very useful for avoiding having to work with signatures
 	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are requried.
-	use self::sr_primitives::{
+	use self::runtime_primitives::{
 		testing::{Digest, DigestItem, Header},
 		traits::{BlakeTwo256, IdentityLookup},
 		BuildStorage,
