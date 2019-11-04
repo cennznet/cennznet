eeef2a58a9bc59d76da72e5b245ed31d761981ef
Eric <e.lim0322@gmail.com>
2019-11-04 16:35:41 +1300

    bump up spec_version 25 -> 26

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -111,7 +111,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("cennznet"),
 	impl_name: create_runtime_str!("centrality-cennznet"),
 	authoring_version: 1,
-	spec_version: 25,
+	spec_version: 26,
 	impl_version: 25,
 	apis: RUNTIME_API_VERSIONS,
 };
