b2ba56ae8ccbc4faec36f5ec026ae4ace1da5ee0
Eric <e.lim0322@gmail.com>
2019-11-28 17:07:12 +1300

    implement BuyFeeAsset and reinstate associated tests

--- crml/cennzx-spot/src/impls.rs
+++ crml/cennzx-spot/src/impls.rs
@@ -17,7 +17,7 @@
 //! Extra CENNZX-Spot traits + implementations
 //!
 use super::Trait;
-use crate::Module;
+use crate::{types::FeeExchange, Module};
 use cennznet_primitives::traits::BuyFeeAsset;
 use primitives::crypto::{UncheckedFrom, UncheckedInto};
 use rstd::{marker::PhantomData, prelude::*};
@@ -53,15 +53,81 @@ fn u64_to_bytes(x: u64) -> [u8; 8] {
 }
 
 impl<T: Trait> BuyFeeAsset<T::AccountId, T::Balance> for Module<T> {
-	type FeeExchange = ();
-	fn buy_fee_asset(_who: &T::AccountId, _amount: T::Balance, _exchange_op: &Self::FeeExchange) -> Result {
-		unimplemented!();
+	type FeeExchange = FeeExchange<T::Balance>;
+	/// Use the CENNZX-Spot exchange to seamlessly buy fee asset
+	fn buy_fee_asset(who: &T::AccountId, amount: T::Balance, exchange_op: &Self::FeeExchange) -> Result {
+		// TODO: Hard coded to use spending asset ID
+		let fee_asset_id: T::AssetId = <generic_asset::Module<T>>::spending_asset_id();
+		println!("{:?}", fee_asset_id);
+		Self::make_asset_swap_output(
+			&who,
+			&who,
+			&T::AssetId::from(exchange_op.asset_id),
+			&fee_asset_id,
+			amount,
+			exchange_op.max_payment,
+			Self::fee_rate(),
+		)
+		.map(|_| ())
+		.map_err(|_| "Failed to charge transaction fees during conversion")
 	}
 }
 
 #[cfg(test)]
 pub(crate) mod impl_tests {
 	use super::*;
+	use crate::{
+		mock::{self, CORE_ASSET_ID, FEE_ASSET_ID, TRADE_ASSET_A_ID},
+		tests::{CennzXSpot, ExtBuilder, Test},
+		types::FeeExchange,
+	};
+	use primitives::H256;
+	use support::traits::Currency;
+
+	type CoreAssetCurrency = mock::CoreAssetCurrency<Test>;
+	type TradeAssetCurrencyA = mock::TradeAssetCurrencyA<Test>;
+	type FeeAssetCurrency = mock::FeeAssetCurrency<Test>;
+
+	#[test]
+	fn buy_fee_asset() {
+		ExtBuilder::default().build().execute_with(|| {
+			with_exchange!(CoreAssetCurrency => 1000, TradeAssetCurrencyA => 1000);
+			with_exchange!(CoreAssetCurrency => 1000, FeeAssetCurrency => 1000);
+
+			let user = with_account!(CoreAssetCurrency => 0, TradeAssetCurrencyA => 100);
+
+			assert_ok!(<CennzXSpot as BuyFeeAsset<_, _>>::buy_fee_asset(
+				&user,
+				51,
+				&FeeExchange::new(TRADE_ASSET_A_ID, 1_000_000),
+			));
+
+			assert_exchange_balance_eq!(CoreAssetCurrency => 946, TradeAssetCurrencyA => 1058);
+			assert_exchange_balance_eq!(CoreAssetCurrency => 1054, FeeAssetCurrency => 949);
+
+			assert_balance_eq!(user, CoreAssetCurrency => 0);
+			assert_balance_eq!(user, TradeAssetCurrencyA => 42);
+		});
+	}
+
+	#[test]
+	fn buy_fee_asset_insufficient_trade_asset() {
+		ExtBuilder::default().build().execute_with(|| {
+			let user = with_account!(CoreAssetCurrency => 0, TradeAssetCurrencyA => 10);
+
+			assert_err!(
+				<CennzXSpot as BuyFeeAsset<_, _>>::buy_fee_asset(
+					&user,
+					51,
+					&FeeExchange::new(TRADE_ASSET_A_ID, 1_000_000)
+				),
+				"Failed to charge transaction fees during conversion"
+			);
+
+			assert_balance_eq!(user, CoreAssetCurrency => 0);
+			assert_balance_eq!(user, TradeAssetCurrencyA => 10);
+		});
+	}
 
 	#[test]
 	fn u64_to_bytes_works() {
