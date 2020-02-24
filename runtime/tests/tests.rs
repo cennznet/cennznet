21103617c5b6a5906d31323c7bfaba82cee7a7fe
Eric <e.lim0322@gmail.com>
2020-02-24 16:09:42 +1300

    Add licence notice

--- runtime/tests/tests.rs
+++ runtime/tests/tests.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2020 Centrality Investments Limited
+// This file is part of CENNZnet.
+//
+// CENNZnet is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CENNZnet is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CENNZnet.  If not, see <http://www.gnu.org/licenses/>.
+
 #![cfg(test)]
 use cennznet_primitives::types::{Balance, FeeExchange, FeeExchangeV1};
 use cennznet_runtime::{
