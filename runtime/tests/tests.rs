3fe10db1654d0f9bd1ec688e4605199f74441c09
Eric <e.lim0322@gmail.com>
2020-03-23 09:34:04 +1300

    Refactor/runtime tests (#160)
    * Remove fee_multiplier param to improve complexity
    - also remove unused Fixed64 trait
    
    * Refactor initialize_block to return Header
    - reason: didn't make sense to wrap Executive::initialize_block
      inside initialize_block while Executive::apply_extrinsic was not
    - outcome: more readable that initialize_block from Executive type
      is being used and Header is passed as argument
    
    * Bump down session_index to reduce loops
    
    * Remove check_free_balance_updated
    - reason: (1) wrapping assert_eq! means test error output doesn't
      report on the test lines that actually fail (hard to debug) and
      (2) the function is not consistently used for other balance asserts
    - outcome: improved consistency and overall complexity

--- runtime/tests/tests.rs
+++ runtime/tests/tests.rs
@@ -18,7 +18,7 @@ use cennznet_primitives::types::{AccountId, Balance, FeeExchange, FeeExchangeV1}
 use cennznet_runtime::{
 	constants::{asset::*, currency::*},
 	Babe, Call, CennzxSpot, CheckedExtrinsic, ContractTransactionBaseFee, EpochDuration, Event, Executive,
-	GenericAsset, Origin, Runtime, Session, SessionsPerEra, Staking, System, Timestamp, TransactionBaseFee,
+	GenericAsset, Header, Origin, Runtime, Session, SessionsPerEra, Staking, System, Timestamp, TransactionBaseFee,
 	TransactionByteFee, TransactionPayment, UncheckedExtrinsic,
 };
 use cennznet_testing::keyring::*;
@@ -35,9 +35,8 @@ use frame_system::{EventRecord, Phase};
 use pallet_contracts::{ContractAddressFor, RawEvent};
 use sp_runtime::{
 	testing::Digest,
-	traits::{Convert, Hash, Header, OnInitialize, SaturatedConversion},
+	traits::{Convert, Hash, Header as HeaderT, OnInitialize, SaturatedConversion},
 	transaction_validity::InvalidTransaction,
-	Fixed64,
 };
 use sp_staking::SessionIndex;
 
@@ -48,14 +47,14 @@ use mock::{validators, ExtBuilder};
 const GENESIS_HASH: [u8; 32] = [69u8; 32];
 const VERSION: u32 = cennznet_runtime::VERSION.spec_version;
 
-fn initialize_block() {
-	Executive::initialize_block(&Header::new(
+fn header() -> Header {
+	HeaderT::new(
 		1,                        // block number
 		sp_core::H256::default(), // extrinsics_root
 		sp_core::H256::default(), // state_root
 		GENESIS_HASH.into(),      // parent_hash
 		Digest::default(),        // digest
-	));
+	)
 }
 
 /// Setup a contract on-chain, return it's deployed address
@@ -71,7 +70,7 @@ fn setup_contract(
 	let wasm = wabt::wat2wasm(contract_wabt).unwrap();
 	let code_hash = <Runtime as frame_system::Trait>::Hashing::hash(&wasm);
 
-	initialize_block();
+	Executive::initialize_block(&header());
 
 	let put_code_call = Call::Contracts(pallet_contracts::Call::put_code(50_000_000, wasm));
 	let put_code_extrinsic = sign(CheckedExtrinsic {
@@ -114,13 +113,15 @@ fn sign(xt: CheckedExtrinsic) -> UncheckedExtrinsic {
 	cennznet_testing::keyring::sign(xt, VERSION, GENESIS_HASH)
 }
 
-fn transfer_fee<E: Encode>(extrinsic: &E, fee_multiplier: Fixed64, runtime_call: &Call) -> Balance {
+fn transfer_fee<E: Encode>(extrinsic: &E, runtime_call: &Call) -> Balance {
 	let length_fee = TransactionByteFee::get() * (extrinsic.encode().len() as Balance);
 
 	let weight = runtime_call.get_dispatch_info().weight;
 	let weight_fee = <Runtime as crml_transaction_payment::Trait>::WeightToFee::convert(weight);
 
 	let base_fee = TransactionBaseFee::get();
+	let fee_multiplier = TransactionPayment::next_fee_multiplier();
+
 	base_fee + fee_multiplier.saturated_multiply_accumulate(length_fee + weight_fee)
 }
 
@@ -168,13 +169,6 @@ fn current_total_payout(validator_count: Balance) -> (Balance, Balance) {
 	(total_payout, max_payout)
 }
 
-fn check_free_balance_updated(account: &AccountId, balance: Balance) {
-	assert_eq!(
-		<GenericAsset as MultiCurrency>::free_balance(account, Some(CENTRAPAY_ASSET_ID)),
-		balance
-	);
-}
-
 fn reward_validators(validators: &[(AccountId, AccountId)]) -> (Balance, Balance) {
 	let validator_len = validators.len() as Balance;
 	let (total_payout, max_payout) = current_total_payout(validator_len);
@@ -201,12 +195,12 @@ fn start_session_works() {
 #[test]
 fn advance_session_works() {
 	ExtBuilder::default().build().execute_with(|| {
-		let session_index = 123;
+		let session_index = 12;
 		start_session(session_index);
 		advance_session();
 		advance_session();
 		advance_session();
-		assert_eq!(Session::current_index(), 126);
+		assert_eq!(Session::current_index(), 15);
 	});
 }
 
@@ -244,7 +238,7 @@ fn current_era_transaction_rewards_storage_update_works() {
 				function: runtime_call_2.clone(),
 			});
 
-			initialize_block();
+			Executive::initialize_block(&header());
 			start_era(1);
 			advance_session(); // advance a session to trigger the beginning of era 2
 			assert_eq!(Staking::current_era(), 2);
@@ -254,12 +248,12 @@ fn current_era_transaction_rewards_storage_update_works() {
 
 			// Apply first extrinsic and check transaction rewards
 			assert!(Executive::apply_extrinsic(xt_1.clone()).is_ok());
-			total_transfer_fee += transfer_fee(&xt_1, TransactionPayment::next_fee_multiplier(), &runtime_call_1);
+			total_transfer_fee += transfer_fee(&xt_1, &runtime_call_1);
 			assert_eq!(Staking::get_current_era_transaction_fee_reward(), total_transfer_fee);
 
 			// Apply second extrinsic and check transaction rewards
 			assert!(Executive::apply_extrinsic(xt_2.clone()).is_ok());
-			total_transfer_fee += transfer_fee(&xt_2, TransactionPayment::next_fee_multiplier(), &runtime_call_2);
+			total_transfer_fee += transfer_fee(&xt_2, &runtime_call_2);
 			assert_eq!(Staking::get_current_era_transaction_fee_reward(), total_transfer_fee);
 
 			// Advancing sessions shouldn't change transaction rewards storage
@@ -294,7 +288,10 @@ fn staking_genesis_config_works() {
 				// Check that RewardDestination is Staked (default)
 				assert_eq!(Staking::payee(&stash), RewardDestination::Staked);
 				// Check validator free balance
-				check_free_balance_updated(&stash, balance_amount);
+				assert_eq!(
+					<GenericAsset as MultiCurrency>::free_balance(&stash, Some(CENTRAPAY_ASSET_ID)),
+					balance_amount
+				);
 				// Check how much is at stake
 				assert_eq!(
 					Staking::ledger(controller),
@@ -335,7 +332,10 @@ fn staking_reward_should_work() {
 			// The balance of stash accounts remain the same within the same era
 			for validator in validators.clone() {
 				let (stash, _) = validator;
-				check_free_balance_updated(&stash, balance_amount);
+				assert_eq!(
+					<GenericAsset as MultiCurrency>::free_balance(&stash, Some(CENTRAPAY_ASSET_ID)),
+					balance_amount
+				);
 			}
 
 			// Advance a session to begin era 2
@@ -349,7 +349,10 @@ fn staking_reward_should_work() {
 			// Staking rewards are paid at the next era
 			for validator in validators {
 				let (stash, _) = validator;
-				check_free_balance_updated(&stash, balance_amount + per_staking_reward);
+				assert_eq!(
+					<GenericAsset as MultiCurrency>::free_balance(&stash, Some(CENTRAPAY_ASSET_ID)),
+					balance_amount + per_staking_reward
+				);
 			}
 		});
 }
@@ -377,8 +380,7 @@ fn staking_validators_should_receive_equal_transaction_fee_reward() {
 				function: runtime_call.clone(),
 			});
 
-			let fm = TransactionPayment::next_fee_multiplier();
-			let fee = transfer_fee(&xt, fm, &runtime_call);
+			let fee = transfer_fee(&xt, &runtime_call);
 			let per_fee_reward = fee / validators.len() as Balance;
 
 			start_era(1);
@@ -390,20 +392,32 @@ fn staking_validators_should_receive_equal_transaction_fee_reward() {
 			assert!(r.is_ok());
 
 			// Check if the transfer is successful
-			check_free_balance_updated(&alice(), balance_amount - transfer_amount - fee);
-			check_free_balance_updated(&bob(), balance_amount + transfer_amount);
+			assert_eq!(
+				<GenericAsset as MultiCurrency>::free_balance(&alice(), Some(CENTRAPAY_ASSET_ID)),
+				balance_amount - transfer_amount - fee
+			);
+			assert_eq!(
+				<GenericAsset as MultiCurrency>::free_balance(&bob(), Some(CENTRAPAY_ASSET_ID)),
+				balance_amount + transfer_amount
+			);
 
 			// Check if stash account balances are not yet changed
 			for validator in validators.clone() {
 				let (stash, _) = validator;
-				check_free_balance_updated(&stash, balance_amount);
+				assert_eq!(
+					<GenericAsset as MultiCurrency>::free_balance(&stash, Some(CENTRAPAY_ASSET_ID)),
+					balance_amount
+				);
 			}
 
 			start_era(2);
 			for validator in validators {
 				let (stash, _) = validator;
 				// Check tx fee reward went to the stash account of validator
-				check_free_balance_updated(&stash, balance_amount + per_fee_reward + per_staking_reward);
+				assert_eq!(
+					<GenericAsset as MultiCurrency>::free_balance(&stash, Some(CENTRAPAY_ASSET_ID)),
+					balance_amount + per_fee_reward + per_staking_reward
+				);
 			}
 		});
 }
@@ -470,22 +484,30 @@ fn generic_asset_transfer_works_without_fee_exchange() {
 		let balance_amount = 10_000 * TransactionBaseFee::get(); // give enough to make a transaction
 		let imbalance = GenericAsset::deposit_creating(&alice(), Some(CENTRAPAY_ASSET_ID), balance_amount);
 		assert_eq!(imbalance.peek(), balance_amount);
-		check_free_balance_updated(&alice(), balance_amount);
+		assert_eq!(
+			<GenericAsset as MultiCurrency>::free_balance(&alice(), Some(CENTRAPAY_ASSET_ID)),
+			balance_amount
+		);
 
 		let xt = sign(CheckedExtrinsic {
 			signed: Some((alice(), signed_extra(0, 0, None, None))),
 			function: runtime_call.clone(),
 		});
 
-		let fm = TransactionPayment::next_fee_multiplier();
-		let fee = transfer_fee(&xt, fm, &runtime_call);
+		let fee = transfer_fee(&xt, &runtime_call);
 
-		initialize_block();
+		Executive::initialize_block(&header());
 		let r = Executive::apply_extrinsic(xt);
 		assert!(r.is_ok());
 
-		check_free_balance_updated(&alice(), balance_amount - transfer_amount - fee);
-		check_free_balance_updated(&bob(), transfer_amount);
+		assert_eq!(
+			<GenericAsset as MultiCurrency>::free_balance(&alice(), Some(CENTRAPAY_ASSET_ID)),
+			balance_amount - transfer_amount - fee
+		);
+		assert_eq!(
+			<GenericAsset as MultiCurrency>::free_balance(&bob(), Some(CENTRAPAY_ASSET_ID)),
+			transfer_amount
+		);
 	});
 }
 
@@ -529,8 +551,7 @@ fn generic_asset_transfer_works_with_fee_exchange() {
 			});
 
 			// Compute the transaction fee of the extrinsic
-			let fm = TransactionPayment::next_fee_multiplier();
-			let fee = transfer_fee(&xt, fm, &runtime_call);
+			let fee = transfer_fee(&xt, &runtime_call);
 
 			// Calculate how much CENNZ should be sold to make the above extrinsic
 			let cennz_sold_amount =
@@ -538,7 +559,7 @@ fn generic_asset_transfer_works_with_fee_exchange() {
 			assert_eq!(cennz_sold_amount, 6);
 
 			// Initialise block and apply the extrinsic
-			initialize_block();
+			Executive::initialize_block(&header());
 			let r = Executive::apply_extrinsic(xt);
 			assert!(r.is_ok());
 
@@ -547,8 +568,14 @@ fn generic_asset_transfer_works_with_fee_exchange() {
 				<GenericAsset as MultiCurrency>::free_balance(&alice(), Some(CENNZ_ASSET_ID)),
 				balance_amount - liquidity_asset_amount - cennz_sold_amount, // transfer fee is charged in CENNZ
 			);
-			check_free_balance_updated(&alice(), balance_amount - liquidity_core_amount - transfer_amount); // transfer fee is not charged in CPAY
-			check_free_balance_updated(&bob(), balance_amount + transfer_amount);
+			assert_eq!(
+				<GenericAsset as MultiCurrency>::free_balance(&alice(), Some(CENTRAPAY_ASSET_ID)),
+				balance_amount - liquidity_core_amount - transfer_amount // transfer fee is not charged in CPAY
+			);
+			assert_eq!(
+				<GenericAsset as MultiCurrency>::free_balance(&bob(), Some(CENTRAPAY_ASSET_ID)),
+				balance_amount + transfer_amount
+			);
 		});
 }
 
@@ -722,7 +749,7 @@ fn contract_call_fails_with_insufficient_gas_without_fee_exchange() {
 		.gas_price(1)
 		.build()
 		.execute_with(|| {
-			initialize_block();
+			Executive::initialize_block(&header());
 			let xt = sign(CheckedExtrinsic {
 				signed: Some((alice(), signed_extra(0, 0, None, None))),
 				function: Call::Contracts(pallet_contracts::Call::call::<Runtime>(
@@ -777,7 +804,7 @@ fn contract_call_fails_with_insufficient_gas_with_fee_exchange() {
 				max_payment: 10 * TransactionBaseFee::get(),
 			});
 
-			initialize_block();
+			Executive::initialize_block(&header());
 			let xt = sign(CheckedExtrinsic {
 				signed: Some((alice(), signed_extra(0, 0, None, Some(fee_exchange)))),
 				function: Call::Contracts(pallet_contracts::Call::call::<Runtime>(
@@ -815,7 +842,7 @@ fn contract_call_works_without_fee_exchange() {
 				signed: Some((alice(), signed_extra(0, 0, None, None))),
 				function: contract_call,
 			});
-			initialize_block();
+			Executive::initialize_block(&header());
 			let r = Executive::apply_extrinsic(xt);
 			assert!(r.is_ok());
 
@@ -868,12 +895,18 @@ fn contract_call_works_with_fee_exchange() {
 				signed: Some((alice(), signed_extra(0, 0, None, Some(fee_exchange)))),
 				function: contract_call,
 			});
-			initialize_block();
+			Executive::initialize_block(&header());
 			let r = Executive::apply_extrinsic(xt);
 			assert!(r.is_ok());
 
-			check_free_balance_updated(&bob(), balance_amount + transfer_amount);
-			check_free_balance_updated(&alice(), balance_amount - transfer_amount);
+			assert_eq!(
+				<GenericAsset as MultiCurrency>::free_balance(&bob(), Some(CENTRAPAY_ASSET_ID)),
+				balance_amount + transfer_amount
+			);
+			assert_eq!(
+				<GenericAsset as MultiCurrency>::free_balance(&alice(), Some(CENTRAPAY_ASSET_ID)),
+				balance_amount - transfer_amount
+			);
 			assert_eq!(
 				<GenericAsset as MultiCurrency>::free_balance(&alice(), Some(CENNZ_ASSET_ID)),
 				9999739653196821,
@@ -916,7 +949,7 @@ fn contract_call_fails_when_fee_exchange_is_not_enough_for_gas() {
 				signed: Some((alice(), signed_extra(0, 0, None, Some(fee_exchange)))),
 				function: contract_call,
 			});
-			initialize_block();
+			Executive::initialize_block(&header());
 			assert_eq!(
 				Executive::apply_extrinsic(xt),
 				Err(InvalidTransaction::Custom(ASSET_TO_CORE_PRICE_ABOVE_MAX_LIMIT).into())
@@ -960,7 +993,7 @@ fn contract_call_fails_when_exchange_liquidity_is_low() {
 				signed: Some((alice(), signed_extra(0, 0, None, Some(fee_exchange)))),
 				function: contract_call,
 			});
-			initialize_block();
+			Executive::initialize_block(&header());
 			assert_eq!(
 				Executive::apply_extrinsic(xt),
 				Err(InvalidTransaction::Custom(INSUFFICIENT_ASSET_RESERVE).into())
@@ -988,7 +1021,7 @@ fn contract_call_fails_when_cpay_is_used_for_fee_exchange() {
 				max_payment: 100 * gas_limit_amount,
 			});
 
-			initialize_block();
+			Executive::initialize_block(&header());
 			let xt = sign(CheckedExtrinsic {
 				signed: Some((alice(), signed_extra(0, 0, None, Some(fee_exchange)))),
 				function: contract_call,
