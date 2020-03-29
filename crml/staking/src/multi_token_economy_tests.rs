a6b0c2a8d3bedbf38a6df29b52fef92665aba637
Roy Sirui Yang <roy.yang@centrality.ai>
2020-03-30 11:08:31 +1300

    Corrected the license headers to update the years and legal texts (#165)
    * Corrected the license headers to update the years and legal texts
    
    * made some changes according to PR comments
    
    * reverted some copyright headers
    
    Co-authored-by: Hoani Bryson <hoani.bryson@gmail.com>

--- crml/staking/src/multi_token_economy_tests.rs
+++ crml/staking/src/multi_token_economy_tests.rs
@@ -1,16 +1,17 @@
-// Copyright 2019-2020 Plug New Zealand Limited
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/* Copyright 2019-2020 Centrality Investments Limited
+*
+* Licensed under the LGPL, Version 3.0 (the "License");
+* you may not use this file except in compliance with the License.
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* You may obtain a copy of the License at the root of this project source code,
+* or at:
+*     https://centrality.ai/licenses/gplv3.txt
+*     https://centrality.ai/licenses/lgplv3.txt
+*/
 
 // Test for staking rewards in a multi-token economic model
 // i.e. The token at stake is not necessarily the token that is rewarded to validators
