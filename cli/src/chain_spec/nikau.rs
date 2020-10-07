ce2de265cfc14a9dbbfcb1fab40bf80a4bc5589e
Jordan Beauchamp <beauchjord@gmail.com>
2020-10-08 10:22:32 +1300

    Reinstate dev + nikau genesis builds Add treasury impl

--- cli/src/chain_spec/nikau.rs
+++ cli/src/chain_spec/nikau.rs
@@ -15,7 +15,7 @@
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
 //! CENNZNet Nikau V1 test net genesis config
-use super::{config_genesis, get_account_id_from_seed, get_authority_keys_from_seed, ChainSpec, NetworkKeys};
+use super::{config_genesis, get_account_id_from_seed, get_authority_keys_from_seed, CennznetChainSpec, NetworkKeys};
 use sp_core::sr25519;
 
 fn network_keys() -> NetworkKeys {
@@ -49,11 +49,12 @@ fn network_keys() -> NetworkKeys {
 	}
 }
 
-/// Returns ChainSpec for Nikau test net
-pub fn config() -> ChainSpec {
-	ChainSpec::from_genesis(
+/// Returns ChainSpec for the Nikau test net
+pub fn config() -> CENNZnetChainSpec {
+	CENNZnetChainSpec::from_genesis(
 		"CENNZnet Nikau",                        // name
 		"CENNZnet Nikau V1",                     // ID
+		ChainType::Live,                         // chain type description
 		|| config_genesis(network_keys(), true), // constructor
 		vec![],                                  // boot nodes
 		None,                                    // telemetry
