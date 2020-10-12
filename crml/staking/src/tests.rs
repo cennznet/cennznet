1d0f5509bb2d62182bcccf8f72193b29fb056797
AlexSedNZ <alex.sedighi@centrality.ai>
2020-10-13 11:15:48 +1300

    Fix on_free_balance_zero_stash_removes_nominator [skip ci]

--- crml/staking/src/tests.rs
+++ crml/staking/src/tests.rs
@@ -1657,6 +1657,9 @@ fn on_free_balance_zero_stash_removes_nominator() {
 		// Check total balance of stash
 		assert_eq!(Balances::total_balance(&11), 0);
 
+		// Reap the stash
+		assert_ok!(Staking::reap_stash(Origin::none(), 11));
+
 		// Check storage items do not exist
 		assert!(!<Ledger<Test>>::contains_key(&10));
 		assert!(!<Bonded<Test>>::contains_key(&11));
