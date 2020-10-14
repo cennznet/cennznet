9d7db33847ac21b17186b55c4ec024c2c50517bb
AlexSedNZ <alex.sedighi@centrality.ai>
2020-10-14 14:06:05 +1300

    Fix chain_spec

--- cli/src/chain_spec/mod.rs
+++ cli/src/chain_spec/mod.rs
@@ -18,12 +18,14 @@
 
 use cennznet_runtime::constants::{asset::*, currency::*};
 use cennznet_runtime::{
-	opaque::SessionKeys, AssetInfo, AuthorityDiscoveryConfig, BabeConfig, CennzxConfig, FeeRate, GenericAssetConfig,
-	GrandpaConfig, ImOnlineConfig, PerMillion, PerThousand, SessionConfig, StakerStatus, StakingConfig, SudoConfig,
-	SystemConfig, WASM_BINARY,
+	opaque::Block, opaque::SessionKeys, AssetInfo, AuthorityDiscoveryConfig, BabeConfig, CennzxConfig, FeeRate,
+	GenericAssetConfig, GrandpaConfig, ImOnlineConfig, PerMillion, PerThousand, SessionConfig, StakerStatus,
+	StakingConfig, SudoConfig, SystemConfig, WASM_BINARY,
 };
 use core::convert::TryFrom;
 use pallet_im_online::sr25519::AuthorityId as ImOnlineId;
+use sc_chain_spec::ChainSpecExtension;
+use serde::{Deserialize, Serialize};
 use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
 use sp_consensus_babe::AuthorityId as BabeId;
 use sp_core::{sr25519, Pair, Public};
@@ -71,8 +73,21 @@ pub struct NetworkKeys {
 	pub root_key: AccountId,
 }
 
+/// Node `ChainSpec` extensions.
+///
+/// Additional parameters for some Substrate core modules,
+/// customizable from the chain spec.
+#[derive(Default, Clone, Serialize, Deserialize, ChainSpecExtension)]
+#[serde(rename_all = "camelCase")]
+pub struct Extensions {
+	/// Block numbers with known hashes.
+	pub fork_blocks: sc_client_api::ForkBlocks<Block>,
+	/// Known bad block hashes.
+	pub bad_blocks: sc_client_api::BadBlocks<Block>,
+}
+
 /// Specialised `ChainSpec`.
-pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig, ()>;
+pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig, Extensions>;
 
 /// Helper function to generate a crypto pair from seed
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
@@ -198,12 +213,38 @@ pub(crate) mod tests {
 	use sp_runtime::BuildStorage;
 
 	fn local_testnet_genesis_instant_single() -> GenesisConfig {
-		testnet_genesis(
-			vec![authority_keys_from_seed("Alice")],
+		let endowed_accounts = vec![
 			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			None,
-			false,
-		)
+			get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+		];
+		let initial_authorities = vec![get_authority_keys_from_seed("Alice")];
+		let root_key = get_account_id_from_seed::<sr25519::Public>("Alice");
+
+		config_genesis(NetworkKeys {
+			endowed_accounts,
+			initial_authorities,
+			root_key,
+		})
+	}
+
+	fn local_testnet_genesis_instant_multi() -> GenesisConfig {
+		let endowed_accounts = vec![
+			get_account_id_from_seed::<sr25519::Public>("Alice"),
+			get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+			get_account_id_from_seed::<sr25519::Public>("Bob"),
+			get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+		];
+		let initial_authorities = vec![
+			get_authority_keys_from_seed("Alice"),
+			get_authority_keys_from_seed("Bob"),
+		];
+		let root_key = get_account_id_from_seed::<sr25519::Public>("Alice");
+
+		config_genesis(NetworkKeys {
+			endowed_accounts,
+			initial_authorities,
+			root_key,
+		})
 	}
 
 	/// Local testnet config (single validator - Alice)
@@ -227,7 +268,7 @@ pub(crate) mod tests {
 			"Integration Test",
 			"test",
 			ChainType::Development,
-			local_testnet_genesis,
+			local_testnet_genesis_instant_multi,
 			vec![],
 			None,
 			None,
@@ -270,16 +311,6 @@ pub(crate) mod tests {
 
 	#[test]
 	fn test_create_development_chain_spec() {
-		development_config().build_storage().unwrap();
-	}
-
-	#[test]
-	fn test_create_local_testnet_chain_spec() {
-		local_testnet_config().build_storage().unwrap();
-	}
-
-	#[test]
-	fn test_staging_test_net_chain_spec() {
-		staging_testnet_config().build_storage().unwrap();
+		dev::config().build_storage().unwrap();
 	}
 }
