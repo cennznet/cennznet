b2e3047904c2cd5f3259e0ed792905862681a4a2
holygits <beauchjord@gmail.com>
2020-12-11 12:08:51 +1300

    impl v38, remove unused code

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -112,7 +112,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// implementation changes and behavior does not, then leave `spec_version` as
 	// is and increment `impl_version`.
 	spec_version: 37,
-	impl_version: 37,
+	impl_version: 38,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
 };
