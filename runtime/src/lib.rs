67d94f7ebbee7c21783227d72a1da6096b21fdb9
AlexSedNZ <alex.sedighi@centrality.ai>
2021-03-08 17:06:44 +1300

    Add wasm_binary_unwrap as in Plug node

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -20,10 +20,6 @@
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 
-// Make the WASM binary available.
-#[cfg(feature = "std")]
-include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
-
 use codec::Encode;
 
 use pallet_authority_discovery;
@@ -102,6 +98,20 @@ use impls::{CurrencyToVoteHandler, RootMemberOnly, SlashFundsToTreasury, WeightT
 pub mod legacy_host_functions;
 
 use crate::opaque::SessionKeys;
+
+// Make the WASM binary available.
+#[cfg(feature = "std")]
+include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+
+/// Wasm binary unwrapped. If built with `SKIP_WASM_BUILD`, the function panics.
+#[cfg(feature = "std")]
+pub fn wasm_binary_unwrap() -> &'static [u8] {
+	WASM_BINARY.expect(
+		"Development wasm binary is not available. This means the client is \
+						built with `SKIP_WASM_BUILD` flag and it is only usable for \
+						production chains. Please rebuild with the flag disabled.",
+	)
+}
 
 /// Runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
