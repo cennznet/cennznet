3d4fa4d0264f53d2c9a29b07d8fb87a2a3955c58
AlexSedNZ <alex.sedighi@centrality.ai>
2021-03-08 16:36:04 +1300

    Update copyright notice

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Parity Technologies (UK) Ltd. and Centrality Investments Ltd.
+// Copyright 2018-2021 Parity Technologies (UK) Ltd. and Centrality Investments Ltd.
 // This file is part of Substrate.
 
 // Substrate is free software: you can redistribute it and/or modify
@@ -15,6 +15,7 @@
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
 //! The CENNZnet runtime. This can be compiled with ``#[no_std]`, ready for Wasm.
+
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
