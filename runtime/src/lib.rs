19e7eab8bbe69f971329f0908cbcd144443431a6
AlexSedNZ <alex.sedighi@centrality.ai>
2021-03-08 16:55:53 +1300

    Bump Runtime versions

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -112,8 +112,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set `impl_version` to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave `spec_version` as
 	// is and increment `impl_version`.
-	spec_version: 38,
-	impl_version: 39,
+	spec_version: 40,
+	impl_version: 40,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
 };
