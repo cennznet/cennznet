7790b0249cb0ed225178cb1a5ee94169b4dc6268
AlexSedNZ <alex.sedighi@centrality.ai>
2021-03-10 11:23:43 +1300

    Fix the test setup for cennzx

--- crml/cennzx/src/mock.rs
+++ crml/cennzx/src/mock.rs
@@ -25,50 +25,47 @@ pub const TRADE_ASSET_B_ID: AssetId = 3;
 /// An asset ID used to pay network fees
 pub const FEE_ASSET_ID: AssetId = 10;
 
+use crate as crml_cennzx;
 use crate::{
 	impls::ExchangeAddressGenerator,
 	types::{FeeRate, PerMillion, PerThousand},
-	Config, GenesisConfig, Module,
+	Config,
 };
 pub(crate) use cennznet_primitives::types::{AccountId, AssetId, Balance};
 use core::convert::TryFrom;
-use frame_support::{impl_outer_event, impl_outer_origin};
+use frame_support::parameter_types;
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
 };
 
-pub type Cennzx = Module<Test>;
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
 
-impl_outer_origin! {
-	pub enum Origin for Test where system = frame_system {}
-}
-
-// alias the Event from this crate under `cennzx` for event testing
-mod cennzx {
-	pub use crate::Event;
-}
-
-impl_outer_event! {
-	pub enum Event for Test {
-		cennzx<T>,
-		frame_system<T>,
-		prml_generic_asset<T>,
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Module, Call, Config, Storage, Event<T>},
+		GenericAsset: prml_generic_asset::{Module, Call, Storage, Config<T>, Event<T>},
+		Cennzx: crml_cennzx::{Module, Call, Storage, Config<T>, Event<T>},
 	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
 }
-
-// For testing the module, we construct most of a mock runtime. This means
-// first constructing a configuration type (`Test`) which `impl`s each of the
-// configuration traits of modules we want to use.
-#[derive(Clone, Eq, PartialEq)]
-pub struct Test;
-
 impl frame_system::Config for Test {
 	type BaseCallFilter = ();
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
 	type Origin = Origin;
 	type Index = u64;
-	type Call = ();
+	type Call = Call;
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
@@ -76,20 +73,14 @@ impl frame_system::Config for Test {
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
 	type Event = Event;
-	type BlockHashCount = ();
-	type MaximumBlockWeight = ();
-	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = ();
-	type AvailableBlockRatio = ();
-	type MaximumBlockLength = ();
+	type BlockHashCount = BlockHashCount;
 	type Version = ();
-	type PalletInfo = ();
+	type PalletInfo = PalletInfo;
 	type AccountData = ();
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
+	type SS58Prefix = ();
 }
 
 impl prml_generic_asset::Config for Test {
@@ -104,9 +95,10 @@ impl Config for Test {
 	type Event = Event;
 	type AssetId = AssetId;
 	type ExchangeAddressFor = ExchangeAddressGenerator<Self>;
-	type MultiCurrency = prml_generic_asset::Module<Self>;
+	type MultiCurrency = GenericAsset;
 	type WeightInfo = ();
 }
+
 pub struct ExtBuilder {
 	core_asset_id: u32,
 	fee_rate: FeeRate<PerMillion>,
@@ -136,7 +128,7 @@ impl ExtBuilder {
 		}
 		.assimilate_storage(&mut t)
 		.unwrap();
-		GenesisConfig::<Test> {
+		crml_cennzx::GenesisConfig::<Test> {
 			core_asset_id: self.core_asset_id,
 			fee_rate: self.fee_rate,
 		}
