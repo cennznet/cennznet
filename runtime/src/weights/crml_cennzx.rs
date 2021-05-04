ba93b99a708e771f0602420dde92dac007043fc4
jenkins <devops@centrality.ai>
2021-05-04 00:11:39 +0000

    add new benchmark files Tue May  4 00:11:39 UTC 2021 with gpg signature

--- runtime/src/weights/crml_cennzx.rs
+++ runtime/src/weights/crml_cennzx.rs
@@ -1,6 +1,6 @@
 //! Weights for crml_cennzx
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0
-//! DATE: 2021-05-03, STEPS: [50], REPEAT: 100, LOW RANGE: [], HIGH RANGE: []
+//! DATE: 2021-05-04, STEPS: [50], REPEAT: 100, LOW RANGE: [], HIGH RANGE: []
 //! EXECUTION: Some(Wasm), WASM-EXECUTION: Compiled, CHAIN: Some("dev"), DB CACHE: 128
 #![allow(unused_parens)]
 #![allow(unused_imports)]
@@ -11,27 +11,27 @@ use sp_std::marker::PhantomData;
 pub struct WeightInfo<T>(PhantomData<T>);
 impl<T: frame_system::Trait> crml_cennzx::WeightInfo for WeightInfo<T> {
 	fn buy_asset() -> Weight {
-		(309_959_000 as Weight)
+		(306_369_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(9 as Weight))
 			.saturating_add(T::DbWeight::get().writes(6 as Weight))
 	}
 	fn sell_asset() -> Weight {
-		(307_901_000 as Weight)
+		(304_841_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(9 as Weight))
 			.saturating_add(T::DbWeight::get().writes(6 as Weight))
 	}
 	fn add_liquidity() -> Weight {
-		(216_954_000 as Weight)
+		(214_865_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(8 as Weight))
 			.saturating_add(T::DbWeight::get().writes(6 as Weight))
 	}
 	fn remove_liquidity() -> Weight {
-		(226_067_000 as Weight)
+		(223_553_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(8 as Weight))
 			.saturating_add(T::DbWeight::get().writes(6 as Weight))
 	}
 	fn set_fee_rate() -> Weight {
-		(12_494_000 as Weight)
+		(12_444_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(1 as Weight))
 			.saturating_add(T::DbWeight::get().writes(1 as Weight))
 	}
--- runtime/src/weights/crml_nft.rs
+++ runtime/src/weights/crml_nft.rs
@@ -1,6 +1,6 @@
 //! Weights for crml_nft
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0
-//! DATE: 2021-05-03, STEPS: [50], REPEAT: 100, LOW RANGE: [], HIGH RANGE: []
+//! DATE: 2021-05-04, STEPS: [50], REPEAT: 100, LOW RANGE: [], HIGH RANGE: []
 //! EXECUTION: Some(Wasm), WASM-EXECUTION: Compiled, CHAIN: Some("dev"), DB CACHE: 128
 #![allow(unused_parens)]
 #![allow(unused_imports)]
@@ -11,47 +11,47 @@ use sp_std::marker::PhantomData;
 pub struct WeightInfo<T>(PhantomData<T>);
 impl<T: frame_system::Trait> crml_nft::WeightInfo for WeightInfo<T> {
 	fn create_collection() -> Weight {
-		(78_012_000 as Weight)
+		(77_601_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(1 as Weight))
 			.saturating_add(T::DbWeight::get().writes(3 as Weight))
 	}
 	fn create_token() -> Weight {
-		(155_574_000 as Weight)
+		(155_250_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(5 as Weight))
 	}
 	fn transfer() -> Weight {
-		(143_359_000 as Weight)
+		(139_117_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(1 as Weight))
 	}
 	fn burn() -> Weight {
-		(165_674_000 as Weight)
+		(162_692_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(5 as Weight))
 			.saturating_add(T::DbWeight::get().writes(3 as Weight))
 	}
 	fn direct_sale() -> Weight {
-		(80_248_000 as Weight)
+		(80_525_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(2 as Weight))
 			.saturating_add(T::DbWeight::get().writes(2 as Weight))
 	}
 	fn direct_purchase() -> Weight {
-		(440_795_000 as Weight)
+		(436_925_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(9 as Weight))
 			.saturating_add(T::DbWeight::get().writes(8 as Weight))
 	}
 	fn auction() -> Weight {
-		(105_275_000 as Weight)
+		(103_982_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(2 as Weight))
 			.saturating_add(T::DbWeight::get().writes(2 as Weight))
 	}
 	fn bid() -> Weight {
-		(181_362_000 as Weight)
+		(178_631_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(8 as Weight))
 			.saturating_add(T::DbWeight::get().writes(5 as Weight))
 	}
 	fn cancel_sale() -> Weight {
-		(109_252_000 as Weight)
+		(106_803_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(3 as Weight))
 			.saturating_add(T::DbWeight::get().writes(2 as Weight))
 	}
