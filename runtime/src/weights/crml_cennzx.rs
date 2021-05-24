a1fe4986962ba87209b36c3416d7185dc71659a4
jenkins <devops@centrality.ai>
2021-05-24 22:57:33 +0000

    Add new benchmark files Mon May 24 22:57:33 UTC 2021

--- runtime/src/weights/crml_cennzx.rs
+++ runtime/src/weights/crml_cennzx.rs
@@ -1,6 +1,6 @@
 //! Weights for crml_cennzx
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0
-//! DATE: 2021-05-05, STEPS: [50], REPEAT: 1000, LOW RANGE: [], HIGH RANGE: []
+//! DATE: 2021-05-24, STEPS: [50], REPEAT: 1000, LOW RANGE: [], HIGH RANGE: []
 //! EXECUTION: Some(Wasm), WASM-EXECUTION: Compiled, CHAIN: Some("dev"), DB CACHE: 128
 #![allow(unused_parens)]
 #![allow(unused_imports)]
@@ -11,27 +11,27 @@ use sp_std::marker::PhantomData;
 pub struct WeightInfo<T>(PhantomData<T>);
 impl<T: frame_system::Trait> crml_cennzx::WeightInfo for WeightInfo<T> {
 	fn buy_asset() -> Weight {
-		(290_171_000 as Weight)
+		(331_370_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(9 as Weight))
 			.saturating_add(T::DbWeight::get().writes(6 as Weight))
 	}
 	fn sell_asset() -> Weight {
-		(289_488_000 as Weight)
+		(332_641_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(9 as Weight))
 			.saturating_add(T::DbWeight::get().writes(6 as Weight))
 	}
 	fn add_liquidity() -> Weight {
-		(203_821_000 as Weight)
+		(233_775_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(8 as Weight))
 			.saturating_add(T::DbWeight::get().writes(6 as Weight))
 	}
 	fn remove_liquidity() -> Weight {
-		(210_695_000 as Weight)
+		(243_114_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(8 as Weight))
 			.saturating_add(T::DbWeight::get().writes(6 as Weight))
 	}
 	fn set_fee_rate() -> Weight {
-		(11_167_000 as Weight)
+		(13_598_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(1 as Weight))
 			.saturating_add(T::DbWeight::get().writes(1 as Weight))
 	}
