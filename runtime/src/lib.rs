d19df7e9ddf36baf7b72aa2b0548914c538ba7f7
holygits <beauchjord@gmail.com>
2022-03-03 14:19:34 +1300

    2.1.0-rc4 / runtime v50 prep release

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -132,8 +132,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set `impl_version` to equal spec_version. If only runtime
 	// implementation chabges and behavior does not, then leave `spec_version` as
 	// is and increment `impl_version`.
-	spec_version: 49,
-	impl_version: 49,
+	spec_version: 50,
+	impl_version: 50,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
 };
