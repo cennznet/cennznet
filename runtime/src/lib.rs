378a5ab79177cd89ecad28098b2172d7e2df7ef3
Jordan <beauchjord@gmail.com>
2022-03-16 18:31:19 -0700

    Client syncing fixes (#602)
    Frontier:
    * Set frontier mapping-sync oldest block in chainSpec
    * Add frontier genesis block numbers
    
    general:
    * Update substrate and frontier version
    Allows client to load db at normal path
    
    bridge:
    * Increase lenience on bride deposit fast-check blocks * 20s -> blocks * 14s
    
    versions:
    * Bump cli 2.1.0-rc7
    Bump runtime v53

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -134,7 +134,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set `impl_version` to equal spec_version. If only runtime
 	// implementation chabges and behavior does not, then leave `spec_version` as
 	// is and increment `impl_version`.
-	spec_version: 52,
+	spec_version: 53,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
