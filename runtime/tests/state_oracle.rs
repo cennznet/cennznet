13ba68510541b6b4ecd6f5e0d3be206c218a4755
holygits <beauchjord@gmail.com>
2022-05-24 20:55:43 +1200

    Fix test

--- runtime/tests/state_oracle.rs
+++ runtime/tests/state_oracle.rs
@@ -5,7 +5,7 @@ use cennznet_runtime::{
 };
 use crml_support::{ContractExecutor, MultiCurrency, H160, U256};
 use fp_rpc::runtime_decl_for_EthereumRuntimeRPCApi::EthereumRuntimeRPCApi;
-use frame_support::traits::OnFinalize;
+use frame_support::{assert_ok, traits::OnFinalize};
 use frame_system::EventRecord;
 use pallet_evm::AddressMapping;
 
@@ -15,29 +15,33 @@ use common::mock::ExtBuilder;
 #[test]
 fn callback_execution() {
 	ExtBuilder::default().build().execute_with(|| {
+        // allows events to be stored
+        System::set_block_number(1);
+
 		let caller = H160::from_low_u64_be(1_u64);
 		let caller_ss58 = <Runtime as pallet_evm::Config>::AddressMapping::into_account_id(caller);
 		let _ = GenericAsset::deposit_creating(&caller_ss58, CPAY_ASSET_ID, 1000 * DOLLARS);
 
 		// Test
-		let result = StateOracleCallbackExecutor::<Runtime>::execute(
+		assert_ok!(StateOracleCallbackExecutor::<Runtime>::execute(
 			&caller,
-			&H160::from_low_u64_be(2_u64),
+            &H160::from_low_u64_be(2_u64),
 			Default::default(),
 			200_000_u64,
 			Runtime::gas_price(),
 			U256::zero(),
-		);
+		));
 		Ethereum::on_finalize(System::block_number());
 
 		// system events has the executed event
-		assert_eq!(System::events().len(), 1_usize);
+        println!("{:?}", System::events());
 		if let EventRecord { 
-            event: Event::Ethereum(pallet_ethereum::Event::Executed(caller, _, _, _)),
+            event: Event::Ethereum(pallet_ethereum::Event::Executed(caller_, _, _, _)),
             phase: _,
             topics: _,
-        } = System::events()[0] {
-			// ethereum pallet current receipts has the callback tx
+        } = System::events().last().unwrap() {
+            assert_eq!(caller, *caller_);
+			// ethereum pallet current receipts contains the callback tx
 			assert_eq!(Ethereum::current_receipts().expect("event exists").len(), 1_usize);
 		} else {
 			assert!(false, "expected evm execution event")
