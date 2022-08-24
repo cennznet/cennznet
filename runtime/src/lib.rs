0e1d009ede09dcf85f9b45aee644db5e22a5a755
holygits <beauchjord@gmail.com>
2022-08-24 15:46:10 +1200

    Bump to 2.2.0-rc7 & runtime v65

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -140,7 +140,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set `impl_version` to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave `spec_version` as
 	// is and increment `impl_version`.
-	spec_version: 64,
+	spec_version: 65,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
